f7fc4e61a8183a522fe584c78902da740e2adf79
DannyAw <kokwah07@hotmail.com>
2017-08-14 16:44:20 +0800

    Added test for new condition(SPH & SPL)

--- test/test_RAW_Operation/test_raw_POP.c
+++ test/test_RAW_Operation/test_raw_POP.c
@@ -11,14 +11,23 @@ void setUp(void){
 
 void tearDown(void){}
 
+/*    Test if the Stack pointer Incremented
+** ---------------------------------------------
+**    condtion 1 : 0x0110 --(INC)--> 0x0111
+*/
 void test_raw_POP_given_addrs_of_SP_0x0110_expected_incremented_by_1_equal_0x0111(void){
   set_SP(0x01,0x10);                             // let SPH = 0x01, SPL = 0x10 => SP 0x0110.
   uint16_t addrsSP = raw_pop(&cpuRegisters->A);
   TEST_ASSERT_EQUAL_HEX16(0x0111,addrsSP);
+  TEST_ASSERT_EQUAL_HEX16(0x01,cpuRegisters->SPH);
+  TEST_ASSERT_EQUAL_HEX16(0x11,cpuRegisters->SPL);
 }
 
+//   conditon 2 : 0xaaff --(INC)--> 0xab00
 void test_raw_POP_given_addrs_of_SP_0xaaff_expected_incremented_by_1_equal_0xab00(void){
   set_SP(0xaa,0xff);                             // let SPH = 0xaa, SPL = 0xff => SP 0xaaff.
   uint16_t addrsSP = raw_pop(&cpuRegisters->A);
   TEST_ASSERT_EQUAL_HEX16(0xab00,addrsSP);
+  TEST_ASSERT_EQUAL_HEX16(0xab,cpuRegisters->SPH);
+  TEST_ASSERT_EQUAL_HEX16(0x00,cpuRegisters->SPL);
 }
