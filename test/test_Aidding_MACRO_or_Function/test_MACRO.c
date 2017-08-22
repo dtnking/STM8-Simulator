756577145819b8c2f513845f8723f88b1412d84c
DannyAW <kokwah07@hotmail.com>
2017-08-23 02:50:27 +0800

    Added test for SET_PC and UPDATE_PC

--- test/test_Aidding_MACRO_or_Function/test_MACRO.c
+++ test/test_Aidding_MACRO_or_Function/test_MACRO.c
@@ -25,3 +25,18 @@ void test_MACRO_getHMSB_given_0x11abcd_expected_result_0x11(void){
   uint8_t result = GET_HMSB(0x11abcd);
   TEST_ASSERT_EQUAL_HEX16(0x11,result);
 }
+
+void test_GET_PC_given_PCE_0x01_PCH_0x02_PCH_0x03_expected_to_get_0x010203(void){
+  cpuRegisters->PCE = 0x01;
+  cpuRegisters->PCH = 0x02;
+  cpuRegisters->PCL = 0x03;
+
+
+  TEST_ASSERT_EQUAL_HEX(0x010203,GET_PC);
+}
+
+void test_UPDATE_PC_given_length_is_2_expected_PC_is_2(void){
+  UPDATE_PC(2);
+
+  TEST_ASSERT_EQUAL_HEX16(0x02,GET_PC);
+}
